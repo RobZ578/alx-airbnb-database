3f337df64a9578d11ecb4846aebb2c5ada32939a
Robel Zeradawit <132105401+RobZ578@users.noreply.github.com>
2025-09-05 22:58:36 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,20 @@
 -- INNER JOIN: Retrieve all bookings and the respective users who made those bookings
 SELECT 
+    b.booking_id,
+    b.property_id,
+    b.user_id,
+    b.start_date,
+    b.end_date,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM bookings b
+INNER JOIN users u 
+    ON b.user_id = u.user_id
+ORDER BY b.booking_id;
+
+-- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
     p.property_id,
     p.title AS property_title,
     r.review_id,
@@ -7,31 +22,20 @@ SELECT
     r.comment
 FROM properties p
 LEFT JOIN reviews r 
-    ON p.property_id = r.property_id;
+    ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
-
-
--- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+-- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
-    p.id AS property_id,
-    p.title AS property_title,
-    r.id AS review_id,
-    r.rating,
-    r.comment
-FROM properties p
-LEFT JOIN reviews r
-    ON p.id = r.property_id;
-
-
--- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking 
--- or a booking is not linked to a user
-SELECT 
-    u.id AS user_id,
-    u.name AS user_name,
-    b.id AS booking_id,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
     b.property_id,
     b.start_date,
     b.end_date
 FROM users u
-FULL OUTER JOIN bookings b
-    ON u.id = b.user_id;
+FULL OUTER JOIN bookings b 
+    ON u.user_id = b.user_id
+ORDER BY u.user_id NULLS LAST, b.booking_id;
